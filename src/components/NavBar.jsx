eaccbec5f4457a5f40f27d5b12007c7139bbd7c7
Antonio Kamiri <antoniokamiri@gmail.com>
2022-06-07 16:36:46 +0300

    NavBar Implementation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,17 +13,34 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => {
   return(
     <TooltipComponent content={title} position='BottomCenter'>
       <button type='button' onClick={customFunc} style= {{color}} className ='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-        <span style={{background : dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>
+        <span style={{background : dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2' />
           {icon}
-        </span>
-
       </button>
     </TooltipComponent>
   )
 }
 
 const NavBar = () => {
-  const {activeMenu, setActiveMenu, handleClick} = useStateContext();
+  const {activeMenu, setActiveMenu, handleClick, IsClicked, setIsClicked, screenSize, setScreenSize} = useStateContext();
+
+  useEffect(() => {
+    const handleResize =  () => setScreenSize (window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect (() => {
+    if(screenSize <= 900){
+      setActiveMenu(false);
+    }else{
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
       <NavButton title='Menu' customFunc={() => setActiveMenu ((prevActiveMenu) => !prevActiveMenu)} color='blue' icon={<AiOutlineMenu />}/>
@@ -41,6 +58,10 @@ const NavBar = () => {
           <MdKeyboardArrowDown className='text-gray-400 text-14'/>
         </div>
       </TooltipComponent>
+      {IsClicked.cart && < Cart />}
+      {IsClicked.chat && < Chat />}
+      {IsClicked.notification && < Notification />}
+      {IsClicked.userProfile && < UserProfile />}
       </div>
     </div>
   )
